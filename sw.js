5853248e51f44967d29c360efc2e3eee024e0b63
Naors23 <naorsadi@gmail.com>
2026-05-13 12:23:20 +0300

    Add service worker with auto-update notification

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'wp916-v1';
+const ASSETS = ['/workplan916/', '/workplan916/index.html', '/workplan916/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+  }
+});
